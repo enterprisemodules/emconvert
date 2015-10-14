a9930934a1cb78fb2b9f41a18437bea2e942bd5d
Bert Hajee <bert.hajee@enterprisemodules.com>
2015-10-14 22:00:51 +0200

    Really fix orautils

--- lib/emconvert/converter.rb
+++ lib/emconvert/converter.rb
@@ -49,9 +49,9 @@ class Converter
     change from:'orawls::utils::orainst', to:'wls_install::utils::orainst'
     change from:'orawls::utils::rcu', to:'wls_install::utils::rcu'
     change from:'orawls::utils::webtier', to:'wls_install::utils::webtier'
-    change from:'orautils::support::files', to:'wls_install::support::files'
-    change from:'orautils::support::nodemanagerautostart',to:'wls_install::support::nodemanagerautostart'
-    change from:'orautils::support::params', to:'wls_install::support::params'
+    change from:'orautils', to:'wls_install::support::files'
+    change from:'orautils::nodemanagerautostart',to:'wls_install::support::nodemanagerautostart'
+    change from:'orautils::params', to:'wls_install::support::params'
     @content
   end
 
